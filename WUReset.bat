45b37c9794f275e1c9a2496f0d844d8321d42582
Emre <stillbirths@gmail.com>
2019-08-13 18:49:25 +0300

    Create WUReset.bat

---
+++ WUReset.bat
@@ -0,0 +1,6 @@
+echo "# Windows-Update-Reset" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/emreozcant/Windows-Update-Reset.git
+git push -u origin master
